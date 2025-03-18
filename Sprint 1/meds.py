b20aeb96a0926d816f929ffdd0e77ba6b7d8a4a8
julion <julionrowland@gmail.com>
2025-03-18 13:30:32 -0400

    updated json io

--- Sprint 1/meds.py
+++ Sprint 1/meds.py
@@ -1,59 +1,72 @@
 import json
-
-
-
-# class meds:
-#     def __init__(self, name="", conditions="", severity=0, time1=[-1, -1], time2=[-1, -1], time3=[-1, -1]):
-#         #first element of time array is hours, second is minutes
-#         #uses 24 hour time scale
-#         #default -1 time means at time hasn't been set for a reminder
-#         self.name = name
-#         self.contidtions = conditions
-#         self.severity = severity
-#         self.time1 = time1
-#         self.time2 = time2
-#         self.time3 = time3
-
-#     def valid(self):
-#         if(self.severity < 0 or self.severity > 5):
-#             return False
-#         if(self.time1[0] < -1 or self.time1[0] > 24 or self.time1[1] < -1 or self.time1[1] > 60):
-#             return False
-#         if(self.time2[0] < -1 or self.time2[0] > 24 or self.time2[1] < -1 or self.time2[1] > 60):
-#             return False
-#         if(self.time3[0] < -1 or self.time3[0] > 24 or self.time3[1] < -1 or self.time3[1] > 60):
-#             return False
-#         else:
-#             return True
             
 class medsManager():
     def __init__(self):
         pass
     def addMedication(self, name="", conditions="", severity=0, time1={-1, -1}, time2={-1, -1}, time3={-1, -1}):
-        self.newMed = {
-            "name" : name,
-            "conditions" : conditions,
-            "severity" : severity,
-            "time1" :{
-                "hours" : time1[0],
-                "minutes" : time1[1]
-            },
-            "time2" :{
-                "hours" : time2[0],
-                "minutes" : time2[1]
-            },
-            "time3" :{
-                "hours" : time3[0],
-                "minutes" : time3[1]
-            },
-        }
+        if(severity < 0 or severity > 5):#returns false if medicaiton information is invalid
+            return False
+        if(time1[0] < -1 or time1[0] > 24 or time1[1] < -1 or time1[1] > 60):
+            return False
+        if(time2[0] < -1 or time2[0] > 24 or time2[1] < -1 or time2[1] > 60):
+            return False
+        if(time3[0] < -1 or time3[0] > 24 or time3[1] < -1 or time3[1] > 60):
+            return False
+        else:#returns true if medicaion is valid and added to list
+            self.newMed = {
+                "name" : name,
+                "conditions" : conditions,
+                "severity" : severity,
+                "time1" :{
+                    "hours" : time1[0],
+                    "minutes" : time1[1]
+                },
+                "time2" :{
+                    "hours" : time2[0],
+                    "minutes" : time2[1]
+                },
+                "time3" :{
+                    "hours" : time3[0],
+                    "minutes" : time3[1]
+                },
+            }
+            self.writeMedication()
+            return True
+        
         #if self.newMed.valid():
+
+    def writeMedication(self):
+        medsArray = []
         try:
-            with open("medsList.json", mode="a", encoding="utf-8") as self.medsList:
-                json.dump(self.newMed, self.medsList, indent = 4)
+            with open("medsList.json", mode="r", encoding="utf-8") as f:
+                #for tempMed in self.medsList:
+                try:
+                    medsArray = json.load(f)
+                except json.decoder.JSONDecodeError:
+                    pass
+                    #print(tempMed)
+                medsArray.append(self.newMed)
+                #print(medsArray)
         except FileNotFoundError:
             print(f"File 'medsList.json' not found! Aborting")
             exit(1)
+        f.close()
         
+        try:
+            with open("medsList.json", mode="w", encoding="utf-8") as self.medsList:
+                #print(medsArray)
+                json.dump(medsArray, self.medsList, indent = 4)
+        except FileNotFoundError:
+            print(f"File 'medsList.json' not found! Aborting")
+            exit(1)
+        self.medsList.close()
             #.dumps(self.newMed, indent = 4)
-                
+
+    def clear(self):
+        try:
+            with open("medsList.json", mode="w", encoding="utf-8") as self.medsList:
+                pass
+        except FileNotFoundError:
+            print(f"File 'medsList.json' not found! Aborting")
+            exit(1)
+        self.medsList.close()
