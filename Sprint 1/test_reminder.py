d8a7ed6993fb9d8670a3f4e16071eab0789a882d
Connor Fair <zapfair@icloud.com>
2025-03-26 17:33:12 -0400

    minor issue with the test code for the reminder class has been fixed

--- Sprint 1/test_reminder.py
+++ Sprint 1/test_reminder.py
@@ -39,7 +39,7 @@ class TestReminder(unittest.TestCase):
         self.reminder.check(test_med)
         self.assertFalse(self.reminder.reminderSent)
 
-
+    # test to ensure that the pop-up works after multiple calls
     def test_case9(self):
         test_med = {"name": "test",
                     "conditions": "many O_O",
@@ -62,13 +62,10 @@ class TestReminder(unittest.TestCase):
                         "hours": self.local_time[0],
                         "minutes": self.local_time[1],
                     }}
-        # I am setting reminderSent to false each time because the class doesn't automaticly reset it yet
         self.reminder.check(test_med)
         self.assertTrue(self.reminder.reminderSent)
-        self.reminder.reminderSent = False
         self.reminder.check(test_med2)
         self.assertTrue(self.reminder.reminderSent)
-        self.reminder.reminderSent = False
         self.reminder.check(test_med3)
         self.assertTrue(self.reminder.reminderSent)
 
