3180e25eea571b1773fc487ca580525c6cbfdc27
Connor Fair <zapfair@icloud.com>
2025-04-04 18:04:13 -0400

    doctors can add and remove patients to their lists

--- Sprint 1/handle_accounts.py
+++ Sprint 1/handle_accounts.py
@@ -107,7 +107,9 @@ class PatientAccount(_Account):
 class DoctorAccount(_Account):
     def __init__(self):
         super().__init__("allDoctors.json")
+        self.patient_list = []
 
+    # methods for handling the doctor account
     def create_account(self, name: str, password: str, phone_number: str, email: str, institution_name: str) -> None:
         """creates a new account and saves it in allDoctors.json.
         :param name: The name of the new account.
@@ -124,6 +126,11 @@ class DoctorAccount(_Account):
                                "email": email,
                                "institution name": institution_name}
         super().create_account(new_doctor_account)
+
+    def find_account(self, name: str, password: str) -> bool:
+        output = super().find_account(name, password)
+        self.patient_list = self._get_patient_list()
+        return output
 
     def update_account(self, name="", password="", phone_number="", email="", institution_name="") -> None:
         """Updates an existing account and saves it in allDoctors.json.
@@ -142,13 +149,93 @@ class DoctorAccount(_Account):
         # replaces all updated records in the currently stored account
         super().update_account(filtered_changes)
 
+    # methods for handling the patient list
+    def __get_patient_doc_list(self) -> list:
+        all_records = []
+        try:
+            with open("doctorPatients.json", mode="r", encoding="utf-8") as f:
+                try:
+                    all_records = json.load(f)
+                except json.decoder.JSONDecodeError:
+                    pass
+        except FileNotFoundError:
+            print(f"File 'doctorPatients.json' not found! Aborting")
+            exit(1)
+        f.close()
+        return all_records
+
+    def _get_patient_list(self) -> list[int]:
+        """find the record with the same ID as the doctor to get the list of Patient id's
+        :return: list of Patient id's."""
+        all_records = self.__get_patient_doc_list()
+        all_keys = [list(i.keys())[0] for i in all_records]
+        try:
+            index = all_keys.index(str(self.account["id"]))
+            return list(all_records[index].values())[0]
+        except ValueError:
+            return []
+
+    def __update_patient_list(self) -> None:
+        # gets all doctor to patient pairings and puts them in a list
+        all_patients = self.__get_patient_doc_list()
+        all_keys = [list(i.keys())[0] for i in all_patients]
+        # add the updated patient list to the list
+        try:
+            index = all_keys.index(str(self.account["id"]))
+            # if the doctor already has patients, update the existing list
+            if len(self.patient_list) == 0:
+                # if the doctor has no more patients, remove them from the list
+                all_patients.pop(index)
+            else:
+                all_patients[index][str(self.account["id"])] = self.patient_list
+        except ValueError:
+            # if the doctor doesn't have patients, append the doctor's list to the end of the file
+            all_patients.append({str(self.account["id"]):self.patient_list})
+
+        with open("doctorPatients.json", mode="w", encoding="utf-8") as f:
+            json.dump(all_patients, f, indent=4)
+            f.close()
+
+    def add_patient(self, name: str) -> bool:
+        # gets all the patients from the allPatients.json
+        all_patients = []
+        try:
+            with open("allPatients.json", mode="r", encoding="utf-8") as f:
+                try:
+                    all_patients = json.load(f)
+                except json.decoder.JSONDecodeError:
+                    pass
+        except FileNotFoundError:
+            print(f"File 'allPatients.json' not found! Aborting")
+            exit(1)
+        f.close()
+        # finds the name in the list, if found the id is added to the list and true is returned
+        # else nothing is added and false is returned
+        all_names = [r["name"] for r in all_patients]
+        try:
+            index = all_names.index(name)
+            self.patient_list.append(index)
+            self.__update_patient_list()
+        except ValueError:
+            return False
+        return True
+
+    def remove_patient(self, pid: int) -> None:
+        self.patient_list.remove(pid)
+        self.__update_patient_list()
+
+
+
 if __name__ == "__main__":
     temp = DoctorAccount()
-    temp.create_account("temp Docman", "password",
-                        "9876543210", "email@web.com",
-                        "hell")
+    temp.find_account("Alice Morgan", "securePass123!")
     print(temp.account)
+    print(temp.patient_list)
+    temp.add_patient("Sophia Martinez")
+    print(temp.patient_list)
+    temp.remove_patient(5)
+    print(temp.patient_list)
     #temp.update_account(email="no@maybe.net")
     #print(temp.account)
-    temp.delete_account()
-    print(temp.account)
+    # temp.delete_account()
+    # print(temp.account)
