2c957e85a3a29e7e76129b8b906854e303495a2f
Connor Fair <zapfair@icloud.com>
2025-04-21 12:20:29 -0400

    all string verification methods work

--- Sprint 3/str_verification.py
+++ Sprint 3/str_verification.py
@@ -1,14 +1,14 @@
 import re
 def valid_name(name: str) -> bool:
     """Verifies the name is in the form: 'First Last'."""
-    return bool(re.match("[A-Z][a-z]* [A-Z][a-z]*"), name)
+    return bool(re.match("[A-Z][a-z]* [A-Z][a-z]*", name))
 
 def valid_password(password: str) -> bool:
     """Ensures the password is al least 8 characters long and contains a number and 
     a special character."""
     if len(password) < 8:
         return False
-    return bool(re.match(".*[0-9].*"), password) and bool(re.match(".*[()+_\-@#$%\^&*].*", password))
+    return bool(re.match(".*[0-9].*", password)) and bool(re.match(".*[()+_\-@#$%\^&*].*", password))
 
 def valid_phone_num(phone_num: str) -> bool:
     """Ensures the phone number is in one of these forms: xxxxxxxxxx, (xxx)xxx-xxxx"""
@@ -30,31 +30,18 @@ def valid_conditions(conditions: str) -> bool:
 
 def valid_severity(severity: str) -> bool:
     """Ensures the severity is between 1 and 5 inclusive"""
-    return bool(re.match("[1-5]", severity))
+    return bool(re.match("[1-5]", severity)) and len(severity) == 1
 
 def valid_time(time: str) -> bool:
-    """Ensures the time is either: 0≤hour≤23:0≤min≤59 or (AM|PM)1≤hour≤12:0≤min≤59"""
-    if len(time) == 5:
-        hour = time[:2]
-        minute = time[3:]
-        if bool(re.match("[0-2][0-9]", hour)) and bool(re.match("[0-5][0-9]", minute)):
-            hour = int(hour)
-            minute = int(minute)
-            return 0 <= hour <= 23 and 0 <= minute <= 59
-        return False
-    elif len(time) == 7:
-        am_or_pm = time[:2]
-        hour = time[2:4]
-        minute = time[5:]
-    if bool(re.match("[0-2][0-9]", hour)) and bool(re.match("[0-5][0-9]", minute)):
-        hour = int(hour)
-        minute = int(minute)
-        return 1 <= hour <= 12 and 0 <= minute <= 59 and am_or_pm in ["AM", "am", "PM", "pm"]
+    """Ensures the time is in the form 0≤hour≤23:0≤min≤59 """
+    if bool(re.match("[0-9]{1,2}:[0-9]{1,2}", time)):
+        hour, minute = time.split(":")
+        return 3 <= len(time) <= 5 and 0 <= int(hour) <= 23 and 0 <= int(minute) <= 59
     return False
 
 def valid_birthday(birthday: str) -> bool:
     """Ensures the birthday is in the form: MM-DD-YYYY."""
-    if bool(re.match("[0-1][0-9]-[0-3][0-9]-[0-2][0-9]{3}", birthday)):
+    if bool(re.match("[0-1][0-9]-[0-3][0-9]-[0-2][0-9]{3}", birthday)) and len(birthday) == 10:
         month, day, year = birthday.split("-")
         return 1 <= int(month) <= 12 and 1 <= int(day) <= 31
     return False
