6fd7be188b3c1d0930ec3a449ea71c82ae1a0de8
julion <julionrowland@gmail.com>
2025-04-25 16:17:21 -0400

    report updated in main

--- Sprint 3/patient.py
+++ Sprint 3/patient.py
@@ -1,5 +1,6 @@
 from meds import medsManager
 import os
+import time
 
 class patient():
     def __init__(self, first="", last="", phone="", insurance="", birthday=[00,00,0000]):
@@ -49,8 +50,8 @@ class patient():
     def reminderCheck(self):
         self.medsManager.reminderCheck()
 
-    def generateReport(self):
-        self.medsManager.generateReport()
+    def generateReport(self, medName: str = None, time1: time.struct_time = None, time2: time.struct_time = None):
+        self.medsManager.generateReport(medName, time1, time2)
     
     def clearReport(self):
         self.medsManager.clearReport()
